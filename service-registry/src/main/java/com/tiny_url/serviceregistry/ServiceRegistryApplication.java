8f92c4e25fb97a7a9dd729a4d87227aeaed1d4a3
HoroiVasile <horoi.vasile@stud.ubbcluj.ro>
2024-01-23 20:56:12 +0200

    added service registry

---
+++ service-registry/src/main/java/com/tiny_url/serviceregistry/ServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.tiny_url.serviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+
+}
