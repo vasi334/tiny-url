bc6d3d9a4e2389b2260bdc06d10e09de5c551c13
HoroiVasile <horoi.vasile@stud.ubbcluj.ro>
2024-01-28 12:47:53 +0200

    added hash-service

---
+++ hash-service/src/main/java/com/tiny_url/hashservice/HashServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tiny_url.hashservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HashServiceApplication.class, args);
+	}
+
+}
